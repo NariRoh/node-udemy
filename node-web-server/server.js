bddba6ae7ae4cf3b339c081a9ef8769b5edc95b3
Nari Roh <nari.rohh@gmail.com>
2018-08-07 23:26:40 -0500

    Rendering templates with data

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const hbs = require('hbs')
 
 var app = express();
 
+// This lets us set some various express related configurations
+app.set('view engine', 'hbs');
 // To add a middleware: app.use takes the middleware function you want to use
   // express.static takes the absolute path to the folder you want to serve up
   // __dirname stores the path to your projects directory
@@ -11,14 +14,19 @@ app.use(express.static(__dirname + '/public'));
 app.get("/", (req, res) => {
 	// res.send("<h1>Hello Express!</h1>");
 	// To send JSON object
-	res.send({
-		name: "Nari",
-		likes: ["Biking", "Cities"]
-	});
+  res.render("home.hbs", {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website',
+    currentYear: new Date().getFullYear()
+  })
 });
 
 app.get("/about", (req, res) => {
-	res.send("About page");
+  // render lets you render any of the templates you have set up with your current view engine
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
 });
 
 app.get("/bad", (req, res) => {
