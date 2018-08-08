78dfd9884799090dd062a9a2bada2ddcd83e9623
Nari Roh <nari.rohh@gmail.com>
2018-08-08 00:25:04 -0500

    Express middleware

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const hbs = require("hbs");
+const fs = require("fs");
 
 var app = express();
 
@@ -7,18 +8,38 @@ hbs.registerPartials(__dirname + "/views/partials");
 // This lets us set some various express related configurations
 app.set("view engine", "hbs");
 // To add a middleware: app.use takes the middleware function you want to use
-// express.static takes the absolute path to the folder you want to serve up
 // __dirname stores the path to your projects directory
-app.use(express.static(__dirname + "/public"));
 
-// first arg: name of helper, second arg: function 
-hbs.registerHelper('getCurrentYear', () => {
-  return new Date().getFullYear();
+// next exists so you can tell express that when your middleware function is done
+// req: everything comes from the client
+app.use((req, res, next) => {
+	var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+	next();
 });
 
-hbs.registerHelper('screamIt', (text) => {
-  return text.toUpperCase();
-})
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+// express.static takes the absolute path to the folder you want to serve up
+app.use(express.static(__dirname + "/public"));
+
+// first arg: name of helper, second arg: function
+hbs.registerHelper("getCurrentYear", () => {
+	return new Date().getFullYear();
+});
+
+hbs.registerHelper("screamIt", text => {
+	return text.toUpperCase();
+});
 // HTTP get request: url, function to run
 app.get("/", (req, res) => {
 	// To send JSON object
