f8bb869f5e9e159a4f3c3a6b415ce85062699cc3
Nari Roh <nari.rohh@gmail.com>
2018-08-10 19:17:23 -0500

    Check indentation-2

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -13,56 +13,56 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 app.post("/todos", (req, res) => {
-	// console.log(req.body);
-	const todo = new Todo({
-		text: req.body.text
-	});
+  // console.log(req.body);
+  const todo = new Todo({
+    text: req.body.text
+  });
 
-	todo.save().then(
-		doc => {
-			res.send(doc);
-		},
-		err => {
-			res.status(400).send(err);
-		}
-	);
+  todo.save().then(
+    doc => {
+      res.send(doc);
+    },
+    err => {
+      res.status(400).send(err);
+    }
+  );
 });
 
 app.get("/todos", (req, res) => {
-	Todo.find().then(
-		todos => {
-			res.send({ todos });
-		},
-		err => {
-			res.status(400).send(err);
-		}
-	);
+  Todo.find().then(
+    todos => {
+      res.send({ todos });
+    },
+    err => {
+      res.status(400).send(err);
+    }
+  );
 });
 
 // GET /todos/id
 app.get("/todos/:id", (req, res) => {
-	const id = req.params.id;
+  const id = req.params.id;
 
-	// valid id using isValid
-	if (!ObjectID.isValid(id)) {
-		return res.status(404).send(); // send with no data
-	}
+  // valid id using isValid
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send(); // send with no data
+  }
 
-	Todo.findById(id)
-		.then(todo => {
-			if (!todo) {
-				return res.status(404).send();
-			}
+  Todo.findById(id)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
 
-			res.send({ todo });
-		})
-		.catch(err => {
-			res.status(400).send();
-		});
+      res.send({ todo });
+    })
+    .catch(err => {
+      res.status(400).send();
+    });
 });
 
 app.listen(3000, () => {
-	console.log("Started on port 3000");
+  console.log("Started on port 3000");
 });
 
 module.exports = { app };
