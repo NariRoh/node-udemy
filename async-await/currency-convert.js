da27268c6d8c911442c0f1fa19086fe85d698ddc
Nari Roh <nari.rohh@gmail.com>
2018-08-11 21:43:16 -0500

    Currency convert app - 2

--- async-await/currency-convert.js
+++ async-await/currency-convert.js
@@ -28,12 +28,29 @@ const getExchangeRate = async (from, to) => {
 const getCountries = async currencyCode => {
     const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
     return response.data.map(country => country.name);
-}
+};
 
-getExchangeRate('USD', 'CAD').then(rate => {
-    console.log(rate);
+// const convertCurrency = (from, to, amount) => {
+//     let convertedAmount;
+
+//     return getExchangeRate(from, to)
+//         .then(rate => {
+//             convertedAmount = (amount * rate).toFixed(2);
+//             return getCountries(to);
+//         })
+//         .then(countries => {
+//             return `${amount} ${from} is worth ${convertedAmount} ${to}. You can spend it in the following countries: ${countries} ${amount} ${from} is worth ${convertedAmount} ${to}. You can spend it in the following countries: ${countries.join(', ')}`;
+//     });
+// };
+
+const convertCurrency = async (from, to, amount) => {
+    const rate = await getExchangeRate(from, to);
+    const countries = await getCountries(to);
+    const convertedAmount = (amount * rate).toFixed(2);
+
+    return `${amount} ${from} is worth ${convertedAmount} ${to}. You can spend it in the following countries: ${countries} ${amount} ${from} is worth ${convertedAmount} ${to}. You can spend it in the following countries: ${countries.join(", ")}`;
+};
+
+convertCurrency('USD', 'USD', 20).then((message) => {
+    console.log(message);
 });
-
-getCountries('CAD').then(countries => {
-    console.log(countries);
-})
