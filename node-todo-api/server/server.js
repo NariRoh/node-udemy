b72c435dd610ca442dd83c5355e0b64672e22fc2
Nari Roh <nari.rohh@gmail.com>
2018-08-15 00:34:00 -0500

    Add POST /users/login route

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -149,6 +149,20 @@ app.get('/users/me', authenticate, (req, res) => {
     // }); in middleware/authenticate.js
 });
 
+// POST /users/login {email, password}
+app.post('/users/login', (req, res) => {
+    const body = _.pick(req.body, ["email", "password"]);
+    // res.send(body);
+    User.findByCredentials(body.email, body.password).then(user => {
+        return user.generateAuthToken().then(token => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch(err => {
+        res.status(400).send(); 
+    })
+
+});
+
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
 });
